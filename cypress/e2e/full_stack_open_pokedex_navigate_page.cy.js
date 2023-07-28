58a32a01d2f4d33548bf6d1ac98cfb717fe00c64
Mishael <magsanoc@live.com>
2023-07-28 12:20:40 -0400

    update ivysaur test

--- cypress/e2e/full_stack_open_pokedex_navigate_page.cy.js
+++ cypress/e2e/full_stack_open_pokedex_navigate_page.cy.js
@@ -1,8 +1,7 @@
 describe('Pokedex', function() {
   it('front page navigates to another page', function() {
     cy.visit('http://localhost:5000')
-    cy.contains('ivysaur')
-    cy.visit('http://localhost:5000/pokemon/ivysaur')
+    cy.contains('ivysaur').click()
     cy.contains('chlorophyll')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
