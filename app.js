212508935a23d42fd7b37c3e02332ca996e7c4b5
Mishael <magsanoc@live.com>
2023-08-01 16:52:48 -0400

    update app for deployment

--- app.js
+++ app.js
@@ -9,3 +9,7 @@ app.use(express.static('dist'))
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
+
+app.get('/version', (req, res) => {
+  res.send('New Version is deployed!') // change this string to ensure a new version deployed
+})
